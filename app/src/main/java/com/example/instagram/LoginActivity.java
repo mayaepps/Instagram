c9172dd0bceccd330e1e8769f81d88d9fffdb04a
Maya Epps <mepps@fb.com>
2020-07-06 22:28:41 -0700

    Implement log in feature
    - Use Parse database to log in
    - Improve UI for login screen
    - Change from the login to main activities when the user successfully logs in
    - Prevent the user from going back to the login screen with the back button after entering the main screen

--- app/src/main/java/com/example/instagram/LoginActivity.java
+++ app/src/main/java/com/example/instagram/LoginActivity.java
@@ -2,14 +2,19 @@ package com.example.instagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.renderscript.ScriptGroup;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.instagram.databinding.ActivityLoginBinding;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -20,12 +25,18 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // If someone is already logged in, go straight to the main activity
+        if (ParseUser.getCurrentUser() != null) {
+            goMainActivity();
+        }
+
         // view binding
         final ActivityLoginBinding binding = ActivityLoginBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
 
         setContentView(view);
 
+        // When login button clicked, get the username and password to log in
         binding.btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,12 +45,31 @@ public class LoginActivity extends AppCompatActivity {
                 loginUser(username, password);
             }
         });
-
-
     }
 
     private void loginUser(String username, String password) {
-        Log.i(TAG, username + " " + password);
-        // todo
+        // preferred to log in in the background because it's asynchronous (done on a background thread)
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                // If the request is successful, the exception will be null
+
+                if (e != null) {
+                    Log.e(TAG, "Issue with login: " + e.getMessage(), e);
+                    return;
+                } else {
+                    goMainActivity();
+                    Toast.makeText(LoginActivity.this, "Login successful!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    // Navigate to the main activity now that the user has signed in
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        // Can't use the back button to navigate back to the login activity
+        finish();
     }
 }
--- app/src/main/java/com/example/instagram/ParseApplication.java
+++ app/src/main/java/com/example/instagram/ParseApplication.java
@@ -10,5 +10,13 @@ public class ParseApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
+        // set applicationId, and server server based on the values in the Heroku settings.
+        // clientKey is not needed unless explicitly configured
+        // any network interceptors must be added with the Configuration Builder given this syntax
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.application_id)) // should correspond to APP_ID env variable
+                .clientKey(getString(R.string.master_key))  // set explicitly unless clientKey is explicitly configured on Parse server
+                .server(getString(R.string.server)).build());
+
     }
 }

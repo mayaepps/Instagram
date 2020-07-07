3ca02e339803413754a10890338adaf9d8e9cce9
Maya Epps <mepps@fb.com>
2020-07-06 17:35:25 -0700

    Improved UI and created ParseApplication.java
    - Added Instagram logo
    - Created ParseApplication and added it to Android Manifest

---
+++ app/src/main/java/com/example/instagram/ParseApplication.java
@@ -0,0 +1,14 @@
+package com.example.instagram;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}

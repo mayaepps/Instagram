d7f9aabaf9813958f038b7a62568ecc362308842
Maya Epps <mepps@fb.com>
2020-07-07 22:46:07 -0700

    Set up image capture and post creation
    - Use implicit intent to go to camera
    - Retrive image from camera through fileProvider
    - Add ability to add caption
    - When submit button is tapped, put photo and caption in Parse database

--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -1,24 +1,41 @@
 package com.example.instagram;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.instagram.databinding.ActivityLoginBinding;
 import com.example.instagram.databinding.ActivityMainBinding;
 import com.parse.FindCallback;
+import com.parse.Parse;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
+import java.io.File;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42;
+    private ActivityMainBinding binding;
+    private File photoFile;
+    private String photoFileName = "photo.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // view binding
-        final ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
 
         setContentView(view);
@@ -46,16 +63,82 @@ public class MainActivity extends AppCompatActivity {
 
                 ParseUser currentUser = ParseUser.getCurrentUser();
 
-                savePost(description, currentUser);
+                if (photoFile == null || binding.ivPostImage.getDrawable() == null) {
+                    // The image preview or the photoFile is null so it should not post the image
+                    Toast.makeText(MainActivity.this, "There is no image to post!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                savePost(description, currentUser, photoFile);
+            }
+        });
+
+        binding.btnCaptureImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchCamera();
             }
         });
 
     }
 
-    private void savePost(String description, ParseUser currentUser) {
+    private void launchCamera() {
+        // create implicit Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider, required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(MainActivity.this, "com.codepath.fileprovider", photoFile);
+        // Tell the camera application where to put the output using fileProvider
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {  // URI => Unifrom resourse Identifier
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        return new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+    }
+
+    private void savePost(String description, ParseUser currentUser, File image) {
         Post post = new Post();
         post.setDescription(description);
+        post.setImage(new ParseFile(image));
         post.setUser(currentUser);
+        post.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    // Something has gone wrong
+                    Log.e(TAG, "Error while saving post to Parse: " + e.getMessage());
+                    return;
+                }
+                Log.i(TAG, "Post save was successful");
+
+                // Clear out the views to indicate the post was submitted
+                binding.etDescription.setText("");
+                binding.ivPostImage.setImageResource(0); // resource id of 0 is an empty resource
+            }
+        });
     }
 
     private void queryPosts() {
@@ -77,5 +160,22 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    // When the camera comes back with a picture
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                binding.ivPostImage.setImageBitmap(takenImage);
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
